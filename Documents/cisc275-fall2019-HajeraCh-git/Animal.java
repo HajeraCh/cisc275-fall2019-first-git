778c343a95c408fcc048be43a1ac4c02a5261f6f
Hajera Chaudhri <hajerachaudhri@wifi-roaming-128-4-112-29.host.udel.edu>
2019-09-12 19:14:28 -0400

    commit animal java file

---
+++ Documents/cisc275-fall2019-HajeraCh-git/Animal.java
@@ -0,0 +1,18 @@
+
+
+public class Animal {
+	
+	public String name;
+	public int legs;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getLegs() {
+		return this.legs;
+	}
+	
+}
+
+
